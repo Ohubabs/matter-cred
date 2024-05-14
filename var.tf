c02dad656594b730a0d1ab33e4accc82640a1cef
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-05-14 11:21:27 -0700

    Create var.tf

---
+++ var.tf
@@ -0,0 +1,19 @@
+variable "ichiraku" {
+    description = "AWS Acces ID"
+    default = "" 
+}
+
+variable "ramen" {
+    description = "AWS Secret Key"
+    default = "" 
+}
+
+variable "spicy" {
+    description = "Username for Grafana Dashboard"
+    default = "Princebabs" 
+}
+
+variable "noodles" {
+    description = "Password for Grafana Dashboard"
+    default = "" 
+}
